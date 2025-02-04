a88d1dff3b729821c6037290e8aa7ce0515da0c4
Devithaun <devithaun@gmail.com>
2025-02-04 22:59:38 +0000

    update repo impl to provide cached photos

--- data/src/main/java/com/devithaun/data/repository/PhotoRepositoryImpl.kt
+++ data/src/main/java/com/devithaun/data/repository/PhotoRepositoryImpl.kt
@@ -4,6 +4,9 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.devithaun.data.db.PhotoDao
+import com.devithaun.data.db.toPhotoEntities
+import com.devithaun.data.db.toPhotos
 import com.devithaun.data.network.PhotoApi
 import com.devithaun.domain.model.Photo
 import com.devithaun.domain.repository.PhotoRepository
@@ -12,14 +15,25 @@ import javax.inject.Inject
 
 class PhotoRepositoryImpl @Inject constructor(
     private val api: PhotoApi,
-    private val dataStore: DataStore<Preferences>
+    private val dataStore: DataStore<Preferences>,
+    private val photoDao: PhotoDao
 ) : PhotoRepository {
 
     companion object {
         private val KEY = stringPreferencesKey("filter")
     }
 
-    override suspend fun getPhotos(): List<Photo> = api.getPhotos()
+    override suspend fun getPhotos(): List<Photo> {
+        val cachedPhotos = photoDao.getAllPhotos()
+        return if (cachedPhotos.isNotEmpty()) {
+            cachedPhotos.toPhotos()
+        } else {
+            val photos = api.getPhotos()
+            photoDao.insertPhotos(photos.toPhotoEntities())
+            photos
+
+        }
+    }
 
     override suspend fun saveUserFilter(author: String) {
         dataStore.edit {

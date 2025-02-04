bb2960031965cf2059a4835a8b6ace7783216bf5
Devithaun <devithaun@gmail.com>
2025-02-04 19:09:18 +0000

    update MainActivity to show PhotoListScreen | remove unneeded Composable

--- app/src/main/java/com/devithaun/lorempicsum/MainActivity.kt
+++ app/src/main/java/com/devithaun/lorempicsum/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.devithaun.lorempicsum.ui.theme.LoremPicsumTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.devithaun.lorempicsum.ui.screens.PhotoListScreen
+import com.devithaun.lorempicsum.viewmodel.PhotosViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,30 +15,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoremPicsumTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val viewModel: PhotosViewModel = hiltViewModel()
+            PhotoListScreen(viewModel = viewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoremPicsumTheme {
-        Greeting("Android")
     }
 }

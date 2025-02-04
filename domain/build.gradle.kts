e215eba577a33791fc5b94daf4c5ba73f20a350e
Devithaun <devithaun@gmail.com>
2025-02-04 14:24:23 +0000

    add datastore version to toml | add hilt to domain layer

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+
 }
 
 android {
@@ -40,4 +42,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // DI (Hilt)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
